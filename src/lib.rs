e02ec0d3ece819129af5461952b7111b35bfb13c
alyssarosedev <alyssarosedev@protonmail.com>
2021-07-08 22:24:34 -0700

    Beginning of rework and handles

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
-#![no_std]
 #![allow(dead_code)]
+#![warn(rust_2018_idioms)]
+#![warn(missing_docs)]
 #![feature(maybe_uninit_ref)]
 #![feature(maybe_uninit_uninit_array)]
 #![feature(maybe_uninit_extra)]
 
-use core::{
+//! Stele: A Send/Sync Append only data structure
+
+use std::{
+    alloc::{alloc, dealloc, Layout},
     cell::UnsafeCell,
     marker::PhantomData,
     mem::MaybeUninit,
     ops::Index,
     ptr::{null_mut, NonNull},
     sync::atomic::{fence, AtomicPtr, AtomicUsize, Ordering},
-    usize,
+    sync::Arc,
 };
-extern crate alloc;
-use alloc::alloc::{alloc, dealloc, Layout};
-
-const WORD_BITS: usize = core::mem::size_of::<usize>() * 8;
 
 pub(crate) unsafe fn alloc_inner<T>(len: usize) -> *mut crate::Inner<T> {
     if core::mem::size_of::<T>() == 0 {
@@ -35,35 +35,44 @@ pub(crate) unsafe fn dealloc_inner<T>(ptr: *mut crate::Inner<T>, len: usize) {
 #[derive(Debug)]
 pub(crate) struct Inner<T> {
     raw: MaybeUninit<UnsafeCell<T>>,
-    _marker: core::marker::PhantomData<T>,
 }
 
 impl<T> Inner<T> {
     pub(crate) fn init(val: T) -> Self {
         let init: MaybeUninit<UnsafeCell<T>> = MaybeUninit::new(UnsafeCell::new(val));
-        Self {
-            raw: init,
-            _marker: PhantomData,
-        }
+        Self { raw: init }
     }
 
-    //SAFETY: The caller of this function must ensure the index
-    //is inbounds and valid for the underlying Inner
-    pub(crate) unsafe fn read(&self) -> &T {
-        self.raw.assume_init_ref().get().as_ref().unwrap()
+    pub(crate) fn read(&self) -> &T {
+        //SAFETY: Creating an Inner requires initialization, and the
+        //value cannot be overwritten or deleted without dropping the
+        //whole object, so assuming initialization and providing an
+        //immutable reference is always safe
+        unsafe { self.raw.assume_init_ref().get().as_ref().unwrap() }
+    }
+}
+
+impl<T> Inner<T>
+where
+    T: Copy,
+{
+    pub(crate) fn get(&self) -> T {
+        unsafe { *self.raw.assume_init_ref().get() }
     }
 }
 #[derive(Debug)]
+/// Stele is an array of atomic pointers with an implicit capacity
+/// of (1<<n)-1 where n is the index in the outer array
 pub struct Stele<T> {
-    inners: [AtomicPtr<Inner<T>>; WORD_BITS],
+    inners: [AtomicPtr<Inner<T>>; 48],
     len: AtomicUsize,
 }
 
-fn split_idx(idx: usize) -> (usize, usize) {
+const fn split_idx(idx: usize) -> (usize, usize) {
     match idx {
         0 => (0, 0),
         _ => {
-            let outer_idx = WORD_BITS - idx.leading_zeros() as usize;
+            let outer_idx = 48 - (idx.leading_zeros() - 16) as usize;
             let inner_idx = 1 << outer_idx - 1;
             (outer_idx, idx - inner_idx)
         }
@@ -79,58 +88,41 @@ const fn max_len(n: usize) -> usize {
 
 impl<T> Stele<T> {
     const NULL_ATOMIC: AtomicPtr<Inner<T>> = AtomicPtr::new(null_mut());
+    /// New returns a Stele with a length and capacity of zero, and the atomic pointers null
     pub fn new() -> Self {
         Stele {
-            inners: [Stele::NULL_ATOMIC; WORD_BITS],
+            inners: [Stele::NULL_ATOMIC; 48],
             len: AtomicUsize::new(0),
         }
     }
-
+    unsafe fn read_raw(&self, idx: usize) -> *mut Inner<T> {
+        let (oidx, iidx) = split_idx(idx);
+        self.inners[oidx].load(Ordering::Relaxed).add(iidx)
+    }
+    /// Read returns a reference to the value at the index, and panics on an out-of-bounds index
     pub fn read(&self, idx: usize) -> &T {
         assert!(self.len.load(Ordering::Relaxed) > idx);
-        let (oidx, iidx) = crate::split_idx(idx);
         //SAFETY: The assertion validates that this value exists and is initialized
-        unsafe {
-            self.inners[oidx]
-                .load(Ordering::Relaxed)
-                .add(iidx)
-                .as_ref()
-                .unwrap()
-                .read()
-        }
+        unsafe { self.read_raw(idx).as_ref().unwrap().read() }
     }
-
+    /// Try_Read attempts to read at the index provided, returning None for an index that is invalid
     pub fn try_read(&self, idx: usize) -> Option<&T> {
         let len = self.len.load(Ordering::Acquire);
         if len < idx {
             return None;
         } else {
-            let (oidx, iidx) = split_idx(idx);
-            Some(unsafe {
-                self.inners[oidx]
-                    .load(Ordering::Acquire)
-                    .add(iidx)
-                    .as_ref()?
-                    .read()
-            })
+            //SAFETY: The if block ensures that this value exists and is initialized
+            unsafe { Some(self.read_raw(idx).as_ref().unwrap().read()) }
         }
     }
-
-    pub fn write(&self, val: T) {
+    /// Push takes a value and appends it the the Stele
+    /// allocating if necessary to accomodate a new block of data
+    pub fn push(&self, val: T) {
         let idx = self.len.fetch_add(1, Ordering::AcqRel);
         let (oidx, iidx) = crate::split_idx(idx);
+        //SAFETY: Allocating new blocks
         unsafe {
-            if idx == 0 || idx == 1 {
-                self.inners[oidx]
-                    .compare_exchange(
-                        null_mut(),
-                        alloc_inner(1),
-                        Ordering::AcqRel,
-                        Ordering::Relaxed,
-                    )
-                    .unwrap();
-                *self.inners[oidx].load(Ordering::Acquire) = Inner::init(val);
-            } else if idx.is_power_of_two() && !(idx == 0 || idx == 1) {
+            if idx.is_power_of_two() || idx == 0 || idx == 1 {
                 self.inners[oidx]
                     .compare_exchange(
                         null_mut(),
@@ -148,6 +140,15 @@ impl<T> Stele<T> {
     }
 }
 
+impl<T: Copy> Stele<T> {
+    /// Get provides a way to get an owned copy of a value inside a Stele
+    /// provided the T implements copy
+    pub fn get(&self, idx: usize) -> T {
+        assert!(self.len.load(Ordering::Relaxed) > idx);
+        unsafe { (*self.read_raw(idx)).get() }
+    }
+}
+
 impl<T> Index<usize> for Stele<T> {
     type Output = T;
 
@@ -160,71 +161,48 @@ impl<T> Drop for Stele<T> {
     fn drop(&mut self) {
         let l = self.len.get_mut();
         let n_ptrs = (l.next_power_of_two() - 1).count_ones() as usize;
-        let _: () = unsafe {
-            (0..=n_ptrs)
-                .map(|n| match n {
+        unsafe {
+            for n in 0..=n_ptrs {
+                match n {
                     0 | 1 => dealloc_inner(*self.inners[n].get_mut(), 1),
                     _ => dealloc_inner(*self.inners[n].get_mut(), max_len(n)),
-                })
-                .collect()
+                }
+            }
         };
     }
 }
+struct WriteHandle<T> {
+    data: Arc<Stele<T>>,
+}
 
 struct ReadHandle<'a, T> {
+    inner: Arc<AtomicPtr<Stele<T>>>,
     _marker: PhantomData<&'a Stele<T>>,
 }
-
-impl<'a, T> IntoIterator for ReadHandle<'a, T> {
-    type Item = &'a T;
-
-    type IntoIter = SteleIter<'a, T>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        todo!()
-    }
-}
-
-struct SteleIter<'a, T> {
-    _marker: PhantomData<&'a Stele<T>>,
-}
-
-impl<'a, T> Iterator for SteleIter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!()
-    }
-}
-
-#[cfg(test)]
-#[macro_use]
-extern crate std;
-#[cfg(test)]
-use std::prelude::v1::*;
-
 mod test {
-
+    #[allow(unused_imports)]
+    use super::*;
     #[test]
     fn write_test() {
-        let s: crate::Stele<usize> = crate::Stele::new();
-        let _: () = (0..1 << 16).map(|n| s.write(n)).collect();
-        s.read(0);
+        let s: Stele<usize> = Stele::new();
+        let _: () = (0..1 << 16)
+            .map(|n| {
+                s.push(n);
+            })
+            .collect();
+        assert_eq!(s.len.load(std::sync::atomic::Ordering::Acquire), 1 << 16);
     }
 
     #[test]
     fn write_zst() {
-        let s: crate::Stele<()> = crate::Stele::new();
-        let _: () = (0..256).map(|_| s.write(())).collect();
+        let s: Stele<()> = Stele::new();
+        let _: () = (0..256).map(|_| s.push(())).collect();
     }
 
-    // #[test]
-    // fn split_test() {
-    //     let _: () = (0..256)
-    //         .map(|n: usize| {
-    //             let (o, i) = crate::split_idx(n);
-    //             println!("index {}, outer {}, inner {}", n, o, i)
-    //         })
-    //         .collect();
-    // }
+    #[test]
+    fn getcopy() {
+        let s: Stele<u8> = Stele::new();
+        s.push(0);
+        assert_eq!(s.get(0), 0);
+    }
 }
