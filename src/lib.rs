681a59270a2c0701178e16a48879a7adafe7acdb
AlyssaRoseDev <alyssarosedev@protonmail.com>
2024-02-07 23:52:45 -0800

    Fix double export

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,6 @@ extern crate alloc;
 #[cfg_attr(doc, doc(cfg(not(feature = "allocator_api"))))]
 pub mod append;
 
-#[cfg(any(not(feature = "allocator_api"), doc))]
-#[cfg_attr(doc, doc(cfg(not(feature = "allocator_api"))))]
-pub use append;
 #[cfg(any(feature = "allocator_api", doc))]
 #[cfg_attr(doc, doc(cfg(feature = "allocator_api")))]
 ///The Allocator API compatible Stele implementation
